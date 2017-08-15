beed517bf9fb4012f9081c1678e328c2fb8aee6d
averageAZNguy <averageAZNguy@gmail.com>
2017-08-15 10:43:57 -0500

    update server.js

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 var express = require('express');
 var app = express();
-var port = 8080;
-app.get('/',function(req,res){
-	var ips = req.headers['x-forwarded-for'] //||  req.connection.remoteAddress;
-	var connection = req.connection;
-	console.log("headers",typeof req.headers['accept-language'])
-	res.end(ips);
-})
+var getClientAddress;
 
+// Get client IP address from request object ----------------------
+getClientAddress = function (req) {
+        return (req.headers['x-forwarded-for'] || '').split(',')[0] 
+        || req.connection.remoteAddress;
+};
 
-app.listen(port)
-console.log("The server is starting on port:",port)
+app.use(express.static('public'));
+
+app.get("/", function (request, response) {
+  var client_ip = getClientAddress(request);
+  var lang = request.headers['accept-language'].split(",")[0];
+  var OS = request.headers['user-agent'].split('(')[1].split(")")[0]
+  var api = {
+    ipaddress : client_ip, language: lang, software: OS
+  }
+  var result = JSON.stringify(api)
+  response.end(result)
+ 
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
